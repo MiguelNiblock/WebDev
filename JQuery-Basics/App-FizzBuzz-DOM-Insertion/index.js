b91ded93c6dfffccd507f9fe01d29cff6321b16d
MiguelNiblock <miguelniblock@posteo.net>
2020-06-20 20:11:59 -0700

    fizzbuzz app

---
+++ JQuery-Basics/App-FizzBuzz-DOM-Insertion/index.js
@@ -0,0 +1,62 @@
+// your code here
+
+
+let fizzBuzz = countTo => {
+  // your code here
+
+  let isDivisible = num => {
+
+    if ((num % 3 == 0)&&(num % 5 === 0)) {return 'fizzbuzz'}
+    else if (num % 3 === 0) {return 'fizz'}
+    else if (num % 5 === 0) {return 'buzz'}
+    else {return num}
+  };
+
+  array = [];
+
+  for (let i = 1; i <= countTo; i++) {
+    array.push(isDivisible(i))
+  };
+
+  return array
+};
+
+function isFizzBuzz(seqItem) {
+  if (seqItem === 'fizz') {return 'fizz'}
+  else if (seqItem === 'buzz') {return 'buzz'}
+  else if (seqItem === 'fizzbuzz') {return 'fizzbuzz'}
+}
+
+function fizzBuzzGen() {
+
+  console.log('function is running');
+
+  $('#number-chooser').submit(function(e) {
+    
+    console.log('event triggered');
+
+    e.preventDefault();
+    
+    const countTo = $(this).find('#number-choice').val();
+
+    console.log(countTo);
+
+    let sequence = fizzBuzz(countTo);
+
+    console.log(sequence);
+
+    sequence.forEach(seqItem =>
+
+      $('.js-results').append(
+      `<div class="fizz-buzz-item ${isFizzBuzz(seqItem)}">
+        <span>${seqItem}</span>
+      </div>`
+      )
+    
+    );
+    
+
+  });
+}
+
+$(fizzBuzzGen);
