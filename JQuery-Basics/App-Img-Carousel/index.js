f3f7e3b720e7cd0c10061fcd4f0a2715554359b5
MiguelNiblock <miguelniblock@posteo.net>
2020-07-14 17:44:01 -0700

    fix image carousel

--- JQuery-Basics/App-Img-Carousel/index.js
+++ JQuery-Basics/App-Img-Carousel/index.js
@@ -4,11 +4,11 @@ $('.thumbnail').on('click', function(e) {
   let newAlt = $(img).prop('alt')
   let newSrc = $(img).prop('src')
   
-  // console.log(newAlt);
-  // console.log(newSrc);
-  // console.log($('.hero img').prop('src')
+  console.log(newAlt);
+  console.log(newSrc);
+  console.log($('.hero img').prop('src'))
   
   $('.hero img').attr('src',newSrc)
   $('.hero img').attr('alt',newAlt)
-  )
+  
 });
